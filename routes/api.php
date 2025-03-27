74897d6266db9254fde7dc419a27c25e0dd111c3
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-03-27 17:43:11 +0330

    fix: :bug: fix namespace

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TaskController;
